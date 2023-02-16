1bb17f6117d5321366be323167d9f01848e9dedc
Sharon <sharonwanjiru392@gmail.com>
2023-02-16 10:45:35 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,11 @@
 /**
  *  main - prints "Programming is like building a multilingual
  *  puzzle, follow by a newline."
- *  return "The program should end with the value 0."
+ *  return - prints "The value 0."
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
