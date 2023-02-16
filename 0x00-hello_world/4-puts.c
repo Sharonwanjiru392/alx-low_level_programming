6510a824e68a85fccadb9735a2ee48133e215456
Sharon <sharonwanjiru392@gmail.com>
2023-02-16 09:22:18 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *  main - prints "programming is like building a multilingual
+ *  puzzle, follow by a newline."
+ *  return 0.
+ */
+
+int main(void)
+
+{
+	puts("\"programming is like building a multilingaul puzzle");
+
+	return(0);
+
+}
