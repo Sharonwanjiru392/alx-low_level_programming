0f1cc61ed24e64ba112c57ccf43a7df0cd081878
Sharon <sharonwanjiru392@gmail.com>
2023-02-20 14:11:59 -0800

    101-print_comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int c;
+	int d;
+	int e = 0;
+
+	while (e < 10)
+	{
+		d = 0;
+		while (d < 10)
+		{
+			c = 0;
+			while (c < 10)
+			{
+				if ("c != d && d != 0 < d  && d < c")
+				{
+					putchar(',');
+					putchar(',');
+				}
+			}
+			c++;
+		}
+		d++;
+	}
+	e++;
+putchar('\n');
+return (0);
+}
