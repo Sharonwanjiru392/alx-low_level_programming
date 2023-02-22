c7c9f488bf3818a10d572576a81f08d6e48970ee
Sharon <sharonwanjiru392@gmail.com>
2023-02-22 11:20:41 -0800

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -13,14 +13,14 @@ int main(void)
 	while (i < 50)
 	{
 	if (i == 0)
-	printf(",%ld", j);
+	printf("%ld", j);
 	else if (i == 1)
-	printf(",%ld", k);
+	printf(", %ld", k);
 	else
 	{
-	k + = - j;
+	k + = j;
 	j = k - j;
-	printf(",%ld", k);
+	printf(", %ld", k);
 	}
 	i++;
 	}
