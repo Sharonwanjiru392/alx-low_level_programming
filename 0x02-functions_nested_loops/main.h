c6bfcb669c3b60438714f15aa5604c0d881c4e7e
Sharon <sharonwanjiru392@gmail.com>
2023-02-22 09:03:50 -0800

    8-24

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,6 @@
 int _putchar(char c);
 
-void print_alphabet(void);
-
-void print_alphabet_xundefined
+void print_alphabet_x10(void);
 
 int _islower(int c);
 
@@ -22,4 +20,4 @@ int add(int, int);
 
 void print_to_98(int n);
 
-void print_time_table(int n);
+void print_times_table(int n);
