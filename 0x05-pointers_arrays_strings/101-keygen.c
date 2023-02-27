c78ca11c376e2dea73a2d7afa0130a00da0ff4a7
Sharon <sharonwanjiru392@gmail.com>
2023-02-27 15:22:49 -0800

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include < stdlib.h>
+#include <stdlib.h>
 #include <time.h>
 
 /***
@@ -43,10 +43,14 @@ int main(void)
 				break;
 			}
 		}
+
 		for (index = 0; password[index]; index++)
+		
 		{
 			if (password[index] >= (33 + diff_half2))
+
 			{
+
 				password[index] -= diff_half2;
 
 				break;
