8c978ceb20e1f145e26e44e024429f77d8a03283
Sharon <sharonwanjiru392@gmail.com>
2023-03-06 04:04:23 -0800

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#iclude "main.h"
+
+/**
+ * print_chessbord - Entry point
+ * @a: array
+ * Return: Alawys 0 (success
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, n;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (n = 0; n < 8; n++)
+			_putchar(a[i][n]);
+		_putchar('\n');
+	}
+}
