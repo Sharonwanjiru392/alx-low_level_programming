1010361ba7f87afcebe9bfe82447868ca2cb68d0
Sharon <sharonwanjiru392@gmail.com>
2023-03-16 16:02:39 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -5,7 +5,6 @@
 /**
  * is_digit - checks if a string contains a non_digit char
  * @s: string to be evaluated
- *
  * Return: 0 if a non-digit is found, 1 otherwise
  */
 int is_digit(char *s)
@@ -72,7 +71,6 @@ int main(int argc, char *argv[])
 		result[i] = 0;
 	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-
 		digit1 = s1[len1] - '0';
 		carry = 0;
 		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
