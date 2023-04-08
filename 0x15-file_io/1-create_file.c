803e40bd48e39321af998b27cbe552bee061419b
Sharon <sharonwanjiru392@gmail.com>
2023-04-08 13:19:59 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,12 +11,13 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd, w, len = 0;
+
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_conten[len];)
+		for (len = 0; text_content[len];)
 			len++;
 	}
 
