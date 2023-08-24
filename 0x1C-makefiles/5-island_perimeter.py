c6afedcdc323a00d46c3ee853eea495af87f30ee
Sharon <Sharonwanjiru392@gmail.com>
2023-08-24 00:53:31 -0700

    5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
-"""5-island_perimeter module"""
+"""Define an island perimeter of the measuring function we have."""
 
-def island_perimeter(grid):
-    """Returns the perimeter of the island described in grid"""
-    c = 0
-    length = len(grid) -1
-    width = len(grid[0]) -1
+def island_perimeter(grid)
+"""Return pm of the island.
 
-    for i, r in enumerate(grid):
-        for j, n in enumerate(r):
-            if n == 1:
-                if i == 0 or grid[i - 1][j] != 1:
-                    c += 1
-                if j == 0 or grid[i][j - 1] != 1:
-                    c += 1
-                if j == width or grid[i][j + 1] != 1:
-                    c += 1
-                if i == length or grid[i + 1][j] != 1:
-                    c += 1
-        return c
+Grid reprecents water by zero && land by one.
+"""
+width = len(grid[0])
+height = len(grid)
+edges = 0
+size = 0
+
+for i in range(heigth):
+    for j in range(width):
+        if grid[i][j] == 1:
+            size += 1
+            if (j > 0 and grid[i][j - 1] == 1):
+                edges += 1
+            if (i > 0 and grid[i - 1][j] == 1):
+                edges += 1
+    return size + 4 - edges +2
